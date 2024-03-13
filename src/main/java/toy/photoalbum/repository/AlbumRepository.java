0dd7bb3728483692b7edb3373448e79640b9683a
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-14 05:36:41 +0900

    test: 앨범 레포지토리 CRUD 기능 테스트 구현

--- src/main/java/toy/photoalbum/repository/AlbumRepository.java
+++ src/main/java/toy/photoalbum/repository/AlbumRepository.java
@@ -1,2 +1,9 @@
-package toy.photoalbum.repository;public interface AlbumRepository {
+package toy.photoalbum.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import toy.photoalbum.domain.entity.Album;
+
+@Repository
+public interface AlbumRepository extends JpaRepository<Album, Long> {
 }
--- src/test/java/toy/photoalbum/repository/AlbumRepositoryTest.java
+++ src/test/java/toy/photoalbum/repository/AlbumRepositoryTest.java
@@ -1,4 +1,101 @@
-import static org.junit.jupiter.api.Assertions.*;
+package toy.photoalbum.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import toy.photoalbum.domain.entity.Album;
+
+@Transactional
+@SpringBootTest
 class AlbumRepositoryTest {
-  
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @AfterEach
+    void afterEach() {
+        albumRepository.deleteAll();
+    }
+
+
+    //== 기본 CRUD 기능 테스트 ==//
+
+    @DisplayName("앨범 생성 및 아이디 조회 테스트")
+    @Test
+    void saveAndFindByIdTest() {
+        //given
+        Album album = Album.builder()
+                .name("testAlbum")
+                .build();
+
+        //when
+        Album savedAlbum = albumRepository.save(album);
+        Album findAlbum = albumRepository.findById(savedAlbum.getId()).get();
+
+        //then
+        assertThat(findAlbum.getName()).isEqualTo(savedAlbum.getName());
+    }
+
+    @DisplayName("앨범 목록 조회 테스트")
+    @Test
+    void findAllTest() {
+        //given
+        Album album1 = Album.builder()
+                .name("testAlbum1")
+                .build();
+        Album album2 = Album.builder()
+                .name("testAlbum2")
+                .build();
+
+        albumRepository.save(album1);
+        albumRepository.save(album2);
+
+        //when
+        List<Album> albums = albumRepository.findAll();
+
+        //then
+        assertThat(albums.size()).isEqualTo(2);
+    }
+
+    @DisplayName("앨범 이름 변경 테스트")
+    @Test
+    void updateAlbumNameTest() {
+        //given
+        Album album = Album.builder()
+                .name("testAlbum")
+                .build();
+
+        Album savedAlbum = albumRepository.save(album);
+
+        //when
+        Album findAlbum = albumRepository.findById(savedAlbum.getId()).get();
+        findAlbum.changeName("newAlbum");
+
+        //then
+        assertThat(savedAlbum.getName()).isEqualTo("newAlbum");
+    }
+
+    @DisplayName("앨범 삭제 테스트")
+    @Test
+    void deleteByIdTest() {
+        //given
+        Album album = Album.builder()
+                .name("testAlbum")
+                .build();
+
+        Album savedAlbum = albumRepository.save(album);
+
+        //when
+        albumRepository.deleteById(savedAlbum.getId());
+
+        //then
+        Optional<Album> findAlbum = albumRepository.findById(savedAlbum.getId());
+        assertThat(findAlbum.isEmpty()).isTrue();
+    }
 }
