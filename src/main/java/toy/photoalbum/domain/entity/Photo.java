1bbef01649ae1d695ff136500bb210a32ed306f9
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-14 05:04:36 +0900

    feat: 사진 엔티티 구현

--- src/main/java/toy/photoalbum/domain/entity/Photo.java
+++ src/main/java/toy/photoalbum/domain/entity/Photo.java
@@ -1,2 +1,51 @@
-package toy.photoalbum.domain.entity;public class Photo {
+package toy.photoalbum.domain.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "photo", uniqueConstraints = {@UniqueConstraint(columnNames = "photo_id")})
+public class Photo {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "file_name")
+    private String fileName;
+
+    @Column(name = "file_size")
+    private Integer fileSize;
+
+    @Column(name = "thumbnail_url")
+    private String thumbnailUrl;
+
+    @Column(name = "original_url")
+    private String originalUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "album_id")
+    private Album album;
+
+    @Builder
+    public Photo(String fileName, Integer fileSize, String thumbnailUrl, String originalUrl, Album album) {
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.thumbnailUrl = thumbnailUrl;
+        this.originalUrl = originalUrl;
+        this.album = album;
+    }
 }
