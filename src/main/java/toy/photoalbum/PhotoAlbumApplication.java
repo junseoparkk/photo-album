da00a27dce635cf6350e82e00a336ba647762ba6
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-14 04:44:39 +0900

    initial commit

---
+++ src/main/java/toy/photoalbum/PhotoAlbumApplication.java
@@ -0,0 +1,13 @@
+package toy.photoalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAlbumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy/photoalbum/PhotoAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.photoalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
