79b56a3d40d9e7af7e8103356b51cfc2e9dfcc08
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-14 04:49:41 +0900

    feat: BaseTimeEntity 구현

---
+++ src/main/java/toy/photoalbum/domain/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package toy.photoalbum.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    @Column(name = "create_date")
+    private LocalDateTime createdDate;
+
+    @CreatedDate
+    @Column(name = "update_date")
+    private LocalDateTime updatedDate;
+}
